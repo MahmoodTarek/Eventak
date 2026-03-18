a9666a33802a00761a370f3aaa892d891b005c6e
DELL <dev.mahmoudtarek@gmail.com>
2026-03-18 19:31:32 +0200

    Add `provider` package and implement `LanguageProvider` for dynamic language switching

--- lib/eventak_app.dart
+++ lib/eventak_app.dart
@@ -1,19 +1,25 @@
 import 'package:eventak/l10n/app_localizations.dart';
+import 'package:eventak/provider/language_provider.dart';
 import 'package:eventak/ui/screens/home/home.dart';
 import 'package:eventak/utils/resources/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EventakApp extends StatelessWidget {
   const EventakApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var languageProvider = Provider.of<LanguageProvider>(context);
+
     return MaterialApp(
       title: 'Eventak',
       debugShowCheckedModeBanner: false,
+
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: Locale('ar'),
+      locale: Locale(languageProvider.currentLanguage),
+
       initialRoute: AppRoutes.home,
       routes: {
         AppRoutes.home: (context) => const Home(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:eventak/eventak_app.dart';
+import 'package:eventak/provider/language_provider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(EventakApp());
+
+  runApp(ChangeNotifierProvider<LanguageProvider>(
+      create: (BuildContext context) => LanguageProvider(),
+      child: EventakApp())
+  );
 }
